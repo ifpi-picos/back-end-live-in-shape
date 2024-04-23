9cb1e1954c97195693ffd73c001552cc9188f189
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-04-23 13:26:36 -0300

    Create cadastro_pro.js

---
+++ src/rotas/cadastro_pro.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import Express from 'express';
+import { criptografaSenha } from '../../servicos/senha.js';
+
+const router = Express.Router();
+const prisma = new PrismaClient();
+
+router.post('/', async (req, res) => {
+  try {
+    const { emailPro } = req.body;
+    const { sobreNomePro } = req.body;
+    const {  nomePro } = req.body;
+    const { nascimentoPro } = req.body;
+    const {senhaPro } = req.body;
+    const { cpfPro } = req.body;
+    const { telefonePro } = req.body;
+    const { bio } = req.body;
+    const { diploma } = req.body;
+    const { profissao } = req.body;
+    const senhaCriptografada = criptografaSenha(senhaPro);
+    console.log('senhaCriptografada', senhaCriptografada)
+    const profissional = {  nomePro, emailPro, telefonePro, sobreNomePro, nascimentoPro, cpfPro, bio, diploma, profissao, senhaPro: senhaCriptografada};
+    await prisma.profissinal.create({
+      data: profissional,
+    });
+    res.status(201).send('Usuário salvo com sucesso!');
+  } catch (erro) {
+    console.error(erro);
+    res.status(400).send('erro ao salvar usuario!');
+  }
+});
+
+export default router;
