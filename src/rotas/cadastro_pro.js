58a1186e7d6e924be60976f6ddcb46d9d3a72b83
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-07-28 12:11:46 -0300

    atalização dados diploma bio e profissao

--- src/rotas/cadastro_pro.js
+++ src/rotas/cadastro_pro.js
@@ -7,38 +7,29 @@ const prisma = new PrismaClient();
 
 router.post('/', async (req, res) => {
   try {
-    const { email, sobreNome, nome, nascimento, senha, cpf, telefone, profissao, bio, diploma } = req.body;
+    const { nomePro, sobreNomePro, nascimentoPro, cpfPro, telefonePro, emailPro, senhaPro, bio, diploma, profissao } = req.body;
+    const senhaCriptografada_pro = criptografaSenha_pro(senhaPro);
 
-    // Validação básica dos campos obrigatórios
-    if (!email || !sobreNome || !nome || !nascimento || !senha || !cpf || !telefone || !profissao) {
-      return res.status(400).send('Todos os campos são obrigatórios.');
-    }
-
-    // Criptografia da senha
-    const senhaCriptografada = criptografaSenha_pro(senha);
-    console.log('Senha criptografada:', senhaCriptografada);
-
-    const profissional = {
-      nome,
-      email,
-      telefone,
-      sobreNome,
-      nascimento: new Date(nascimento),
-      cpf,
-      profissao,
-      senha: senhaCriptografada,
-      bio,
-      diploma
+    const profissional = { 
+      nome: nomePro, 
+      sobreNome: sobreNomePro, 
+      nascimento: new Date(nascimentoPro), 
+      cpf: cpfPro, 
+      telefone: telefonePro, 
+      email: emailPro, 
+      senha: senhaCriptografada_pro, 
+      bio, 
+      diploma, 
+      profissao 
     };
-
-    // Tentativa de criação do registro no banco de dados
-    const novoProfissional = await prisma.profissional.create({
+    
+    await prisma.profissional.create({
       data: profissional,
     });
 
-    res.status(201).json(novoProfissional);
+    res.status(201).send('Usuário salvo com sucesso!');
   } catch (erro) {
-    console.error('Erro ao salvar profissional:', erro);
+    console.error(erro);
     res.status(400).send('Erro ao salvar usuário!');
   }
 });
