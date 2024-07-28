c9c40746b1ac1a517952d499aad400d6cbb376fa
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-07-28 15:45:29 -0300

    Update cadastro_pro.js

--- src/rotas/cadastro_pro.js
+++ src/rotas/cadastro_pro.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
 import Express from 'express';
+import { PrismaClient } from '@prisma/client';
 import { criptografaSenha_pro } from '../../servicos/senha_pro.js';
 
 const router = Express.Router();
@@ -7,23 +7,27 @@ const prisma = new PrismaClient();
 
 router.post('/', async (req, res) => {
   try {
-    const { emailPro } = req.body;
-    const { sobreNomePro } = req.body;
-    const { nomePro } = req.body;
-    const { nascimentoPro } = req.body;
-    const { senhaPro } = req.body;
-    const { cpfPro } = req.body;
-    const { telefonePro } = req.body;
-    const senhaCriptografada = criptografaSenha_pro(senhaPro);
-    console.log('senhaCriptografada', senhaCriptografada)
-    const profissional = { nomePro, emailPro, telefonePro, sobreNomePro, nascimentoPro, cpfPro, senhaPro: senhaCriptografada};
+    const { nomePro, sobreNomePro, nascimentoPro, cpfPro, telefonePro, emailPro, senhaPro } = req.body;
+    const senhaCriptografada_pro = criptografaSenha_pro(senhaPro);
+
+    const profissional = {
+      nome: nomePro,
+      sobreNome: sobreNomePro,
+      nascimento: new Date(nascimentoPro),
+      cpf: cpfPro,
+      telefone: telefonePro,
+      email: emailPro,
+      senha: senhaCriptografada_pro
+    };
+
     await prisma.profissional.create({
       data: profissional,
     });
+
     res.status(201).send('Usuário salvo com sucesso!');
   } catch (erro) {
-    console.error(erro);
-    res.status(400).send('erro ao salvar usuario!');
+    console.error('Erro ao salvar usuário:', erro);
+    res.status(400).send('Erro ao salvar usuário!');
   }
 });
 
