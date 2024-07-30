f8e49a5f6185cbec868dec83e3465d5242d13d41
Yuri <capic.2022118isinf0381@aluno.ifpi.edu.br>
2024-07-30 17:37:11 -0300

    corrigindo erro do prisma

--- src/rotas/cadastro_pro.js
+++ src/rotas/cadastro_pro.js
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
     const { cpfPro } = req.body;
     const { telefonePro } = req.body;
     const senhaCriptografadaPro = criptografaSenha_Pro(senhaPro);
-    console.log('senhaCriptografada', senhaCriptografada)
+    console.log('senhaCriptografada', senhaCriptografadaPro)
     const profissional = { nome: nomePro, email: emailPro, telefone: telefonePro, sobreNome: sobreNomePro, nascimento: nascimentoPro, cpf: cpfPro, senhaPro: senhaCriptografadaPro};
     console.log("Profissional:" , profissional)
     await prisma.profissional.create({
