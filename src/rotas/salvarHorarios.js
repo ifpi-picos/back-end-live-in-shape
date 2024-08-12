09e42541555ce4306d15c4f9696fe8617ee4c29d
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-08-11 22:46:00 -0300

    Update salvarHorarios.js

--- src/rotas/salvarHorarios.js
+++ src/rotas/salvarHorarios.js
@@ -3,20 +3,24 @@ import { PrismaClient } from '@prisma/client';
 
 const router = Express.Router();
 const prisma = new PrismaClient();
-
 router.post('/', async (req, res) => {
-    const { diaSemana, horaInicio, horaFim } = req.body;
-    const usuarioId = req.userId; // Use o userId decodificado
-  
-    const disponibilidade = { diaSemana, horaInicio, horaFim, usuarioId };
-  
     try {
-      await prisma.disponibilidade.create({
-        data: disponibilidade,
-      });
-      res.status(201).send('Horário salvo com sucesso!');
+        const { diaSemana, horaInicio, horaFim } = req.body;
+        const usuarioId = req.userId; // Pegando o userId do middleware de autenticação
+
+        console.log('Dados recebidos:', { diaSemana, horaInicio, horaFim, usuarioId });
+
+        const disponibilidade = { diaSemana, horaInicio, horaFim, usuarioId };
+
+        await prisma.disponibilidade.create({
+            data: disponibilidade,
+        });
+        
+        res.status(201).send('Horário salvo com sucesso!');
     } catch (error) {
-      res.status(500).send('Erro ao salvar horário: ' + error.message);
+        console.error('Erro ao salvar horário:', error);
+        res.status(500).send('Erro ao salvar horário.');
     }
-  });
+});
+
 export default router;
