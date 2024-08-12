ad1dbb8da872a4493f93638987fc21a4abb0f61f
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-08-11 21:52:15 -0300

    Update salvarHorarios.js

--- src/rotas/salvarHorarios.js
+++ src/rotas/salvarHorarios.js
@@ -4,33 +4,18 @@ import { PrismaClient } from '@prisma/client';
 const router = Express.Router();
 const prisma = new PrismaClient();
 
-router.post('/salvar-horarios', async (req, res) => {
-    const { horarios } = req.body;
-    const usuarioId = 1; // Substitua isso pelo ID do usuário logado
+router.post('/', async (req, res) => {
+    const { diaSemana } = req.body;
+    const { horaInicio } = req.body;
+    const { horaFim } = req.body;
+    const usuarioId =  localStorage.getItem('usuarioId');
 
-    try {
-        // Limpar horários antigos
-        await prisma.disponibilidade.deleteMany({
-            where: { usuarioId: usuarioId }
-        });
+    const disponibilidade = { diaSemana, horaInicio, horaFim, usuarioId};
 
-        // Adicionar novos horários
-        const horariosSalvos = await Promise.all(horarios.map(horario =>
-            prisma.disponibilidade.create({
-                data: {
-                    diaSemana: horario.diaSemana,
-                    horaInicio: horario.horaInicio,
-                    horaFim: horario.horaFim,
-                    usuarioId: usuarioId
-                }
-            })
-        ));
-
-        res.status(200).send('Horários salvos com sucesso!');
-    } catch (error) {
-        console.error('Erro ao salvar horários:', error);
-        res.status(400).send('Erro ao salvar horários!');
-    }
+    await prisma.disponibilidade.create({
+        data: disponibilidade,
+      });
+      res.status(201).send('Horário salvo com sucesso!');
 });
 
 export default router;
